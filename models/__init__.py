542050b4226a02c29cfb748438aff1a52e298cf0
kokhanovskyi-i <jobseeker042011@gmail.com>
2026-06-12 22:07:47 +0300

    Task complete

--- models/__init__.py
+++ models/__init__.py
@@ -7,3 +7,4 @@ from . import hr_hospital_doctor
 from . import hr_hospital_patient
 from . import hr_hospital_mass_reassign_doctor_wizard
 from . import hr_hospital_visit_report_wizard
+from . import hr_hospital_disease_report_wizard
